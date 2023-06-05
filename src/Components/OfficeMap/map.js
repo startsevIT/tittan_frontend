7026d1b89b15346abcd0c969e2142875db3011c1
konfessor <66369760+konfessor@users.noreply.github.com>
2023-06-06 02:01:45 +0500

    some corrections

--- src/Components/OfficeMap/map.js
+++ src/Components/OfficeMap/map.js
@@ -1,11 +1,92 @@
+
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import reception1 from "./mapImg/ресепшн5.jpg"
+import reception2 from "./mapImg/ресепшн6.jpg"
+import cabGenDir from "./mapImg/кабГенДиректора.jpg"
+import BP1 from "./mapImg/проектировщики-строители8.jpg"
+import BP2 from "./mapImg/проектировщики-строители9.jpg"
+import GR from "./mapImg/GR.jpg"
+import PM1 from "./mapImg/управление персоналом1.jpg"
+import PM2 from "./mapImg/управление персоналом2.jpg"
+import PM3 from "./mapImg/управление персоналом3.jpg"
+import lawyers from "./mapImg/юристы.jpg"
+import kitchen from "./mapImg/кухня.jpg"
+import bigRoom from "./mapImg/болПереговорка.jpg";
+import smallRoom from "./mapImg/малПереговорка.jpg";
+import counteringRoom1 from "./mapImg/бух2.jpg";
+import counteringRoom2 from "./mapImg/бух3.jpg";
+import './carouselSlider.css';
 
-class OfficeMap extends React.Component{
-    render(){
-        return(
-            <div>Карта офиса</div>
-        )
-    }
-}
-
-export default OfficeMap
+class OfficeMap extends React.Component {
+  render(){
+        return ( 
+          <Carousel>
+            
+            <div>
+              <img src={reception1}/>
+              <p class="information">Ресепшен</p>
+            </div>
+            <div>
+              <img src={reception2}/>
+              <p class="information">Ресепшен</p>
+            </div>
+            <div>
+              <img src={counteringRoom1}/>
+              <p class="information">Бухгалтерия</p>
+            </div>
+            <div>
+              <img src={counteringRoom2}/>
+              <p class="information">Бухгалтерия</p>
+            </div>
+            <div>
+              <img src={bigRoom}/>
+              <p class="information">Большая Переговорная</p>
+            </div>
+            <div>
+              <img src={smallRoom}/>
+              <p class="information">Малая Переговорная</p>
+            </div>
+            <div>
+              <img src={cabGenDir}/>
+              <p class="information">Кабинет Ген.Директора</p>
+            </div>
+            <div>
+              <img src={BP1}/>
+              <p class="information">Проектировщики-строители</p>
+            </div>
+            <div>
+              <img src={BP2}/>
+              <p class="information">Проектировщики-строители</p>
+            </div>
+            <div>
+              <img src={GR}/>
+              <p class="information">GR</p>
+            </div>
+            <div>
+              <img src={PM1}/>
+              <p class="information">Управление Персоналом</p>
+            </div>
+            <div>
+              <img src={PM2}/>
+              <p class="information">Управление Персоналом</p>
+            </div>
+            <div>
+              <img src={PM3}/>
+              <p class="information">Управление Персоналом</p>
+            </div>
+            <div>
+              <img src={lawyers}/>
+              <p class="information">Юристы</p>
+            </div>
+            <div>
+              <img src={kitchen}/>
+              <p class="information">Кухня</p>
+            </div>
+          </Carousel>
+        );
+      };
+      
+  }
+export default OfficeMap;
--- src/Components/PersonalAccount/account.js
+++ src/Components/PersonalAccount/account.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import { NavLink } from 'react-router-dom';
 import Avatar from '../1img/avatar.png'
 import ExitIcon from '../1img/exit-icon.svg'
@@ -40,9 +41,31 @@ class Account extends React.Component{
                         <button className='edit-info-btn'>Редактировать</button>
                     </div>
                 </div>
+=======
+import './account.css';
+import exitButton from "../1img/exitButton.svg";
+
+class Account extends React.Component{
+    render(){
+      return(
+            <div className="profile">
+              <div className="profile-image-container">
+                <div className="profile-image">Фотография</div>
+                <button className="profile-button">Редактировать</button>
+              </div>
+              <div className="profile-details">
+                <h2>Фамилия Имя Отчество</h2>
+                <p>Дата рождения:</p>
+                <p>Подразделение:</p>
+                <p>Должность:</p>
+                <p>Электронная почта:</p>
+                <p>Телефон:</p>
+                <button className="profile-details-button">Редактировать</button>
+              </div>
+>>>>>>> e8e7c926a4ed4203854bd699c7d35da91d9ea506
             </div>
         )
     }
 }
 
-export default Account
+export default Account;
