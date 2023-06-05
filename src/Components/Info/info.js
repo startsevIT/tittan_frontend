310997c4c2f34a7b502c423d0cb9dfeee683e9ce
konfessor <66369760+konfessor@users.noreply.github.com>
2023-06-06 02:16:39 +0500

    some changess

--- src/Components/Info/info.js
+++ src/Components/Info/info.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './info.css';
-<<<<<<< HEAD
 import InfoIcon from '../1img/amico.png' 
 import FirstMeetingImg from '../1img/first-meeting.png'
 import MainStagesImg from '../1img/main-stages.png'
 import WorkSubtletiesImg from '../1img/work-subtleties.png'
-=======
->>>>>>> e8e7c926a4ed4203854bd699c7d35da91d9ea506
 
 class Info extends React.Component{
     render(){
@@ -18,7 +15,6 @@ class Info extends React.Component{
                     которая поможет успешно пройти тесты и влиться в рабочий процесс</p>
                     <img src={InfoIcon} className='info-icon'></img>
                 </div>
-<<<<<<< HEAD
                 <div className='cards-wrapper'>
                     <NavLink to='#' className='first-info-card'>
                         <div className='info-card-wrapper'>
@@ -38,14 +34,6 @@ class Info extends React.Component{
                             <p className='info-card-title'>Тонкости работы</p>
                         </div>
                     </NavLink>
-=======
-                <div className='about-company-wrapper'>
-                    <div className='company-description-wrapper'>
-                        <p className='company-description-info'>Основной целью создания ОЭЗ ППТ «Титановая долина» является создание 
-                        современных промышленно-производственных комплексов, способныхобеспечить производство высокотехнологичной 
-                        продукции глубокой промышленной переработки. </p>
-                    </div>
->>>>>>> e8e7c926a4ed4203854bd699c7d35da91d9ea506
                 </div>
             </div>
         )
